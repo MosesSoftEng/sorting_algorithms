c1bba0d8ff43c4d90dd98e037c465396818842ee
Moses Mwangi <ngigemoses1@outlook.com>
2022-07-18 10:18:46 +0300

    Task 0 attempt 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,13 +1,33 @@
-#include "main.h"
+#include "sort.h"
 
 /**
  * bubble_sort - function that sorts an array of integers in ascending order
  * using the Bubble sort algorithm
  *
- * @array: array to be sorted
- * @size: size of array
+ * @array: Array to be sorted
+ * @size: Size of array
  */
-void bubble_sort(int *array, size_t size)
+
+/*
+ * Pseudocode
+ * 1. Loop several times to ensure array is fully sorted
+ * 2.	Loop array from index 0 to second last element
+ * 3.		If element at current index is greater than next element, swap elements
+ */
+void bubble_sort(
+	__attribute__ ((unused)) int *array, /* Pointer */
+	__attribute__ ((unused)) size_t size
+)
 {
-	printf("bubble_sort function called");
+	size_t i, j;
+
+	for (i = 0; i < size; i++)
+		for (j = 0; j < size - 1; j++)
+			if (array[j] > array[j + 1])
+			{
+				int temp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
 }
--- sort.h
+++ sort.h
@@ -1,13 +1,18 @@
-/* #ifdef - preprocessor directive, checks if MACRO is not already defined
- * by #define directive
- */
 #ifndef SORT_H
 #define SORT_H
+/*
+ * #ifdef - preprocessor directive, checks if MACRO is not already defined
+ * by #define directive
+ */
 
-/*Libraries*/
-#include <stdlib.h> /*size_t*/
+/* Libraries */
+#include <stdlib.h> /* size_t library*/
+#include <stdio.h>
 
-/*Functions prototypes*/
+/* Functions prototypes */
 void bubble_sort(int *array, size_t size);
+
+/* Helper Functions */
+void print_array(int *a, int n);
 
 #endif
